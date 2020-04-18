d66d0ce235468e1eed86bf47886423e80849c956
larnin <nicolas-donald.laurent@laposte.net>
2020-04-19 00:40:32 +0200

    Laser

--- Assets/Scripts/MiningBehaviour.cs
+++ Assets/Scripts/MiningBehaviour.cs
@@ -15,12 +15,29 @@ public class MiningBehaviour : MonoBehaviour
     float m_miningTime = 0;
     ResourceItem m_clickedResource = null;
 
+    Animator m_animator = null;
+
+    SpriteRenderer m_laser = null;
+    SpriteRenderer m_laserImpact = null;
+
+    private void Awake()
+    {
+        m_animator = GetComponent<Animator>();
+
+        m_laser = transform.Find("Laser").GetComponent<SpriteRenderer>();
+        m_laserImpact = transform.Find("LaserImpact").GetComponent<SpriteRenderer>();
+
+        UpdateLaser(false);
+    }
+
     void Update()
     {
         if (Input.GetButtonDown(click))
             OnMouseClick();
         if (Input.GetButton(click))
             OnMouseHold();
+        if (Input.GetButtonUp(click))
+            OnMouseRelease();
     }
 
     void OnMouseClick()
@@ -93,11 +110,17 @@ public class MiningBehaviour : MonoBehaviour
             }
             else OnMouseRelease();
         }
+
+        UpdateLaser(true);
     }
 
     void OnMouseRelease()
     {
         m_clickedResource = null;
+
+        m_animator.SetBool("Laser", false);
+
+        UpdateLaser(false);
     }
 
     void StartMining(ResourceItem asteroid)
@@ -110,6 +133,10 @@ public class MiningBehaviour : MonoBehaviour
 
         if (!asteroid.HaveResource())
             return;
+
+        m_animator.SetBool("Laser", true);
+
+        UpdateLaser(true);
 
         m_clickedResource = asteroid;
         m_miningTime = 0;
@@ -133,5 +160,35 @@ public class MiningBehaviour : MonoBehaviour
     void OnInteactStation()
     {
 
+    }
+
+    void UpdateLaser(bool show)
+    {
+        m_laser.gameObject.SetActive(show);
+        m_laserImpact.gameObject.SetActive(show);
+
+        if (!show)
+            return;
+
+        if (m_clickedResource == null)
+        {
+            UpdateLaser(false);
+            return;
+        }
+
+        Vector2 pos = transform.position;
+        Vector2 target = m_clickedResource.transform.position;
+        var dir = target - pos;
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 90;
+
+        float dist = dir.magnitude;
+
+        m_laser.transform.rotation = Quaternion.Euler(0, 0, angle);
+        m_laser.transform.position = new Vector3((target.x + pos.x) / 2.0f, (target.y + pos.y) / 2.0f, m_laser.transform.position.z);
+        m_laser.size = new Vector2(m_laser.size.x, dist);
+
+        m_laserImpact.transform.position = new Vector3(target.x, target.y, m_laserImpact.transform.position.z);
+        m_laserImpact.transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
