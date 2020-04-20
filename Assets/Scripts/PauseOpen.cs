b6180b2d944429d729c505ef82dce4bcb47bb3ec
larnin <nicolas-donald.laurent@laposte.net>
2020-04-21 00:35:02 +0200

    Pause menu

---
+++ Assets/Scripts/PauseOpen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseOpen : MonoBehaviour
+{
+    [SerializeField] GameObject m_pausePrefab;
+
+    GameObject m_pauseObj = null;
+
+    string pauseKey = "Cancel";
+
+    private void Update()
+    {
+        if (m_pauseObj != null)
+            return;
+
+        if (Input.GetButtonDown(pauseKey))
+            m_pauseObj = Instantiate(m_pausePrefab);
+    }
+}
---
+++ Assets/Scripts/PauseUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseUI : MonoBehaviour
+{
+    private void Awake()
+    {
+        Time.timeScale = 0;
+    }
+
+    private void OnDestroy()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void OnBack()
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnMainMenu()
+    {
+        SceneSystem.changeScene("MainMenu");
+    }
+}
